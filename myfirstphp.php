24be5562aec4e89ec15731ac5ad4afd6152c2a92
BenjiChristie <benji.christie@queensu.ca>
2019-03-24 16:56:07 -0400

    how about this

--- myfirstphp.php
+++ myfirstphp.php
@@ -2,16 +2,19 @@
 <html>
 <body>
   <h2>
-    Employee Information
+    Sponsor Info
   </h2>
 <?php
-  $givenName= $_POST["firstname"];   #firstnameis the name of the element in form$surname = $_POST[“lastname”];
-  echo "<h3> Hello $givenName</h3>";
-  $dbh= new PDO('mysql:host=localhost;dbname=ProjectP2', "root", "");
-  $rows = $dbh->query("select company_name from sponsor");
-  foreach($rows as $row) {
-    echo print_r($row);
+  // $givenName= $_POST['firstname'];   #firstnameis the name of the element in form$surname = $_POST[“lastname”];
+  // echo "<h3> Hello $givenName</h3>";
+  $pdo= new PDO('mysql:host=localhost;dbname=ProjectP2', 'root', '');
+  $stmt = $pdo->query('SELECT company_name, sponsor_level FROM company');
+  echo "<table><tr><th>Company</th><th>Tier</th></tr>";
+  while ($row = $stmt->fetch())
+  {
+    echo "<tr><td>".$row["company_name"]."</td><td>".$row["sponsor_level"]."</td></tr>";
   }
 ?>
+</table>
 </body>
 </html>
